e6ad2f706183369b0981cb1d4064ac1cc77d15b0
root <root@splunksoar>
2024-08-27 04:50:33 -0700

    Release notes for version 2.0.3

--- skypeforbusiness_consts.py
+++ skypeforbusiness_consts.py
@@ -1,6 +1,6 @@
 # File: skypeforbusiness_consts.py
 #
-# Copyright (c) 2019-2020 Splunk Inc.
+# Copyright (c) 2019-2024 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
